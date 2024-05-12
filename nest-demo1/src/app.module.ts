2661571b4ef32792451945da903a149b48fcfed9
fll <fllning@163.com>
2024-05-12 17:10:45 +0800

    feat: 通过generate生成各种模块代码

--- nest-demo1/src/app.module.ts
+++ nest-demo1/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
