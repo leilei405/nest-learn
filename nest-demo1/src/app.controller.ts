e520e470cff2b9ff4eaf9ab6025f1bdb639f2a86
fll <fllning@163.com>
2024-05-14 23:56:59 +0800

    feat: nest 调试

--- nest-demo1/src/app.controller.ts
+++ nest-demo1/src/app.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // AppController 的构造器里参数里声明了 AppService 的依赖，就会自动注入
+  // constructor(private readonly appService: AppService) {}
+
+  // 如果不想用构造器注入，也可以属性注入，但是属性注入只能注入一次，不能多次注入
+  @Inject(AppService)
+  private readonly appService: AppService;
+
 
   @Get()
   getHello(): string {
